aa3d9c5a1c0260f61440d1e503d047b1003654be
Jesús Fco Cortés <jfcr@live.com>
2018-10-04 10:51:45 -0500

    facebook Share

--- resources/views/visitor/readBlog.blade.php
+++ resources/views/visitor/readBlog.blade.php
@@ -46,6 +46,12 @@
             <div class="justify-text">{!! $blog->text !!}</div>   
 
             <hr>
+            <br>
+            
+            <div class="fb-share-button" data-href="{{ url()->current()}}" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Compartir</a></div>
+            
+            <br><br>
+            <hr>
 
             {{-- SECCION DE COMENTARIOS --}}
 
@@ -114,6 +120,14 @@
 
 @section('scripts')
 
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.1&appId=951140095064464&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="https://unpkg.com/axios@0.12.0/dist/axios.min.js"></script>
     <script>
--- resources/views/visitor/readEvent.blade.php
+++ resources/views/visitor/readEvent.blade.php
@@ -43,16 +43,47 @@
             <h4> {{ $event->date_from }}</h4>
             <p>{{ $event->resume}}</p>            
 
-            <div class="justify-text">{!! $event->text !!}</div>   
+            @if(Auth::check())
+            <div class="justify-text">{!! $event->description !!}</div>   
+
+
+            @else
+                
+
+            <h3 class="centerText">Inicia Sesión para leer todo el evento</h3>
+
+            <a href="{{ url('lastUrl/LoginFacebook')}}" class="loginFacebookBtn">
+                <button><i class="fa fa-facebook"></i> Iniciar sesión con Facebook</button>
+            </a>
+
+            @endif
+
+
+
+            <hr>
+            <br>
+            
+            <div class="fb-share-button" data-href="{{ url()->current()}}" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Compartir</a></div>
+            
+            <br><br>
+            <hr>
             
 
-           
-
 </section>
+<br>
 
 @endsection
 
 @section('scripts')
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.1&appId=951140095064464&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="https://unpkg.com/axios@0.12.0/dist/axios.min.js"></script>    
